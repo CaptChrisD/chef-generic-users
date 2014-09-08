31ba4ec0b6642c102805ca2c0934144e1f78d6ba
Chris Dutton <chris@rosepointnav.com>
2014-09-07 21:08:13 -0700

    Update name again

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ license          "MIT"
 description      "Sets up user accounts"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.4"
-name             "Generic Users Cookbook"
+name             "chef-generic-users"
